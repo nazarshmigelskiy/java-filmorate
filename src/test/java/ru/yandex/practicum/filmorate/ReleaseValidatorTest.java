8a6f60c95153127c1bf621df43bcf492d3a76a5d
Nazar Shmigelskiy <nazarshmigelskiy@yandex.ru>
2026-06-24 19:45:29 +0300

    Готовый проект+1

--- src/test/java/ru/yandex/practicum/filmorate/ReleaseValidatorTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/ReleaseValidatorTest.java
@@ -9,7 +9,7 @@ import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ReleaseDateValidatorTest {
+class ReleaseValidatorTest {
 
     private ReleaseDateValidator validator;
 
