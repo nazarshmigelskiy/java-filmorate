1ed8402b3d018162783205a460052e5d4beb4561
Nazar Shmigelskiy <nazarshmigelskiy@yandex.ru>
2026-08-04 00:59:09 +0300

    Убрал неиспользуемые и ненужные аннотации, убрал многочисленные обращения в базу в checkDirectors, по аналогии сделал то же самое для checkGenresAndMpa

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Event;
